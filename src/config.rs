bdd3fcbd6cc9ebbedc3a06fc01350d0ec82ce874
hyjk555 <ssxuegangliu@gmail.com>
2025-04-24 10:50:25 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["work.cqmaika.com"];
+pub const RS_PUB_KEY: &str = "ey9TRv306gR1+7q4jWqlA2jozkcN2c4UMnd053O7WA8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
